1bba487515f635be2a6adadfa307c7d638887712
CoderrShyam <157949060+CoderrShyam@users.noreply.github.com>
2024-02-03 15:56:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const scrollToTop = (() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
+
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > window.innerHeight * 0.5 ||
+        document.documentElement.scrollTop > window.innerHeight * 0.5
+      ) {
+        scrollToTopBtn.classList.add("show");
+      } else {
+        scrollToTopBtn.classList.remove("show");
+      }
+    });
+
+    scrollToTopBtn.addEventListener("click", () => {
+      const scrollOptions = {
+        top: 0,
+        behavior: "smooth",
+      };
+
+      if (window.innerWidth > 768) {
+        // For larger screens, smoothly scroll to the right before going up
+        scrollOptions.left = window.innerWidth - 60; // Adjust the value as needed
+      }
+
+      window.scrollTo(scrollOptions);
+    });
+  });
+})();
